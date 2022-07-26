d97be2dd4c9baea4133d75bf4692b86fe72e4b70
younessaitali <younes.aitali95@gmail.com>
2022-07-26 17:47:52 +0100

    feat: set page and add unocss

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts' >
+useHead({
+    title: 'Image to HTML'
+})
+</script>
+
+<template>
+
+    <main class="flex justify-center items-center flex-col">
+        <h1>Choose an image file </h1>
+        <input type="file" accept=".jpeg,.jpg,.png,image/jpeg,image/png" aria-label="upload image button" @change="">
+    </main>
+</template>
